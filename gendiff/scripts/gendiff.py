94ea8b60d477ca361b6c657da88d81b1ac03331b
Максим Шевцов <shevtsov_stv@mail.ru>
2025-02-10 16:19:42 +0300

    Edit enter script

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,11 +2,16 @@
 
 import argparse
 
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(usage='gendiff [-h] first_file second_file',
-                                     description='Compares two configuration files and shows a difference.')
+def main():
+    parser = argparse.ArgumentParser(
+        usage='gendiff [-h] first_file second_file',
+        description='Compares two configuration files and shows a difference.'
+        )
     parser.add_argument('first_file')
     parser.add_argument('second_file')
     args = parser.parse_args()
     print(args)
+
+
+if __name__ == '__main__':
+    main()
