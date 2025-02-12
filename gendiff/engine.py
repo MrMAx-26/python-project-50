bac7f7db2ff3202bb8aec7c732546783f86de8a8
Максим Шевцов <shevtsov_stv@mail.ru>
2025-02-12 17:54:25 +0300

    Refactoring code

--- gendiff/engine.py
+++ gendiff/engine.py
@@ -1,4 +1,16 @@
 import json
+import argparse
+
+def run_gendiff():
+    parser = argparse.ArgumentParser(
+        usage='gendiff [-h] [-f FORMAT] first_file second_file',
+        description='Compares two configuration files and shows a difference.'
+        )
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    parser.add_argument('-f', '--format', help='set format of output')
+    args = parser.parse_args()
+    return args
 
 
 def reading_and_parsing(file) -> dict:
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,17 +1,9 @@
 #!/usr/bin/env python
 
-import argparse
-from gendiff.engine import reading_and_parsing
+from gendiff.engine import reading_and_parsing, run_gendiff
 
 def main():
-    parser = argparse.ArgumentParser(
-        usage='gendiff [-h] [-f FORMAT] first_file second_file',
-        description='Compares two configuration files and shows a difference.'
-        )
-    parser.add_argument('first_file')
-    parser.add_argument('second_file')
-    parser.add_argument('-f', '--format', help='set format of output')
-    args = parser.parse_args()
+    args = run_gendiff()
     print(reading_and_parsing(args.first_file))
     print(reading_and_parsing(args.second_file))
 
