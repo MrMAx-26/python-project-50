6582b921110d097e66f369fc4ff682bf99d055cc
Максим Шевцов <shevtsov_stv@mail.ru>
2025-02-13 17:34:28 +0300

    Add generate diff

--- gendiff/engine.py
+++ gendiff/engine.py
@@ -16,3 +16,23 @@ def run_gendiff():
 def reading_and_parsing(file) -> dict:
     data = json.load(open(file))
     return data
+
+
+def generate_diff(file1, file2) -> str:
+    data1 = reading_and_parsing(file1)
+    data2 = reading_and_parsing(file2)
+    keys = sorted(set(data1.keys()).union(data2.keys()))
+    diff = []
+    for key in keys:
+        if key in data1 and key in data2:
+            if data1[key] != data2[key]:
+                diff.append(f' - {key}: {data1[key]}')
+                diff.append(f' + {key}: {data2[key]}')
+            else:
+                diff.append(f'   {key}: {data1[key]}')
+        elif key in data1:
+            diff.append(f' - {key}: {data1[key]}')
+        elif key in data2:
+            diff.append(f' + {key}: {data2[key]}')
+    result_diff = '{\n' + '\n'.join(diff) + '\n}'
+    return result_diff
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 
 from gendiff.engine import reading_and_parsing, run_gendiff
+from gendiff import generate_diff
 
 def main():
     args = run_gendiff()
     print(reading_and_parsing(args.first_file))
     print(reading_and_parsing(args.second_file))
+    print(generate_diff(args.first_file, args.second_file))
 
 
 if __name__ == '__main__':
