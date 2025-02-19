03fedac76b0fb61fe75a564d7e46acc7e45052d1
Максим Шевцов <shevtsov_stv@mail.ru>
2025-02-19 11:24:43 +0300

    Refactoring code

--- gendiff/engine.py
+++ gendiff/engine.py
@@ -11,11 +11,13 @@ def run_gendiff():
     parser.add_argument('second_file')
     parser.add_argument('-f', '--format', help='set format of output')
     args = parser.parse_args()
-    print_diff(args)
+    get_and_print_diff(args.first_file, args.second_file)
 
 
-def print_diff(args):
-    print(generate_diff(args.first_file, args.second_file))
+def get_and_print_diff(first_file, second_file):
+    diff = generate_diff(first_file, second_file)
+    print(diff)
+    return diff
 
 
 def reading_and_parsing(file) -> dict:
